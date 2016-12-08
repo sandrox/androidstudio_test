6605634e0fe09c172159d046f704048e795b164a
sandrox <teamneo@live.de>
2016-12-08 08:20:54 +0100

    Initial commit

---
+++ app/src/main/java/com/mrappcore/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mrappcore.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//TODO complete the project
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
